4ed4afba5f8eec423982e0f985386eb18d65bf3d
coca-in-a-cola <coca-in-a-cola@protonmail.com>
2021-11-27 02:39:23 +0500

    ALL TASKS DONE!

--- filter.py
+++ filter.py
@@ -1,8 +1,20 @@
 from PIL import Image
 import numpy as np
 
-
 def convert_image_to_mosaic(image, size, gradation_step):
+    """
+    Конвертирует изображение
+
+    // >>> convert_image_to_mosaic(np.array(Image.open("image.jpg")), 12, 8)
+    Вернёт ЧБ мозаику с картинки image.jpg как массив. \
+    С размером "пикселя" 12х12 реальных и 8-ю градациями серого \
+    см. в Первую и последнюю картинку в README
+
+    :param image: numpy массив пикселей изображения
+    :param size: размер блока
+    :param gradation_step: количество градаций серого
+    :return: обработанный numpy массив пикселей изображения
+    """
     for x in range(0, len(image), size):
         for y in range(0, len(image[0]), size):
             image[x:x + size, y:y + size] = get_average_brightness(
@@ -11,6 +23,14 @@ def convert_image_to_mosaic(image, size, gradation_step):
 
 
 def get_average_brightness(block, size, gradation_step):
+    """
+    Ищет среднее ЧБ значение на участке пикселей
+
+    :param block: участок пикселей, в котором будем считать среднее
+    :param size: тот самый шаг, размер одного будущего пикселя
+    :param gradation_step: кол-во градаций серого
+    :return: значение от 0 до 255 - яркость пикселя
+    """
     average_color = (block[:size, :size].sum() / 3) // size ** 2
     return int(average_color // gradation_step) * gradation_step
 
